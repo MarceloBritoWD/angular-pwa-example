02524704f623e2afbf2ef6a45c227a294ace5046
Marcelo brito <marcelobritowd95@gmail.com>
2019-05-09 01:51:47 -0300

    Adding the whole app in one single commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-pwa';
+  actualCat: string;
+
+  constructor(
+    private swUpdate: SwUpdate,
+    private dataService: DataService
+    ) {
+  }
+
+  ngOnInit() {
+    this.getCat();
+
+    this.swUpdate.available.subscribe(event => {
+      console.log('A newer version is now available. Refresh the page now to update the cache');
+    });
+    this.swUpdate.checkForUpdate();
+  }
+
+  getCat() {
+    this.dataService.getCat().subscribe((result) => {
+      this.actualCat = result[0].url;
+    });
+  }
 }
